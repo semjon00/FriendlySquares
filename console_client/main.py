de61de9b44ef877cb81a19ac1b8422a73fbe7bcf
Semjon Kravtsenko <semjon.00@gmail.com>
2024-02-12 00:27:52 +0200

    [WIP] Absolutely minimal functioning game

--- console_client/main.py
+++ console_client/main.py
@@ -1,31 +1,63 @@
 import asyncio
+import json
 import socketio
-
 from constants import DEFAULT_PORT
 
-sio = socketio.AsyncClient()
 
-@sio.event
-async def connect():
-    print('connection established')
+class Client:
+    def __init__(self):
+        self.sio = socketio.AsyncClient()
+        self.sio.on('connect', self.connect)
+        self.sio.on('my_message', self.my_message)
+        self.sio.on('disconnect', self.disconnect)
 
-@sio.event
-async def message(data):
-    print('asda', data)
+    async def __send_stuff_routine(self, data):
+        await self.sio.emit('my_message', json.dumps(data))
 
-async def send_message(data):
-    await sio.emit('message', data)
+    async def send_stuff(self, data):
+        await asyncio.create_task(self.__send_stuff_routine(data))
 
-@sio.event
-async def disconnect():
-    print('disconnected from server')
+    async def connect(self):
+        """What happens on connection"""
+        print('Connection established')
+
+    async def my_message(self, data):
+        print(data)
+
+    async def disconnect(self):
+        print('Disconnected from server')
+
+    async def do_connect(self, where):
+        if ':' not in where:
+            where = where + ':' + str(DEFAULT_PORT)
+        await self.sio.connect(f'http://{where}')
+        await self.send_stuff({'cmd': 'msg', 'msg': 'ok_from_client'})
+        await self.sio.wait()
+
+    async def cmd(self, cmd):
+        match cmd[0]:
+            case 'host':
+                await self.send_stuff({'cmd': 'host'})
+            case 'join':
+                await self.send_stuff({'cmd': 'join', 'game_id': cmd[1]})
+            case 'board':
+                await self.send_stuff({'cmd': 'board'})
+            case 'put':
+                await self.send_stuff({'cmd': 'put',
+                                       'idx': int(cmd[1]), 'pos': (int(cmd[2]), int(cmd[3])), 'rot': int(cmd[4])})
+
+    async def command_loop(self):
+        while True:
+            cmd = input('> ')
+            await self.cmd(cmd.split(' '))
 
 
 async def main():
-    await sio.connect(f'http://localhost:{DEFAULT_PORT}')
-    await send_message('test')
-    await send_message('test')
-    await sio.wait()
+    #where = input('Please enter server address: ')
+    where = 'localhost'
+    c = Client()
+    await asyncio.create_task(asyncio.create_task(c.command_loop()))
+    await c.do_connect(where)
 
 
 if __name__ == '__main__':
--- server/main.py
+++ server/main.py
@@ -1,5 +1,7 @@
-from __future__ import annotations
+# TODO: Thread safety, locks? What's that?
+# TODO: No exception handling, speedrun coding mode let's goo
 
+import asyncio
 import json
 import random
 from aiohttp import web
@@ -7,24 +9,22 @@ import socketio
 
 from constants import DEFAULT_PORT
 
-sio = socketio.AsyncServer()
-app = web.Application()
-sio.attach(app)
 
 class Client:
-    def __init__(self, sid):
-        self.my_uuid = sid
-        sio.enter_room(sid, f'room{self.my_uuid}')
+    def __init__(self, sio, sid):
+        self.__sio = sio
+        self.sid = sid
         self.game = None
+        self.is_op = False
 
     async def send_stuff(self, data):
-        await sio.emit('message', data, room=f'room{self.my_uuid}')
+        await self.__sio.emit('my_message', json.dumps(data), room=f'room{self.sid}')
 
 
 class Game:
     def __init__(self):
         self.p_descriptions, self.p_rotations, self.p_positions = self.generate_pieces()
-        self.players = []
+        self.players: list[Client] = []
 
         self.h, self.w = 5, 7
         self.field: list[list[int | str]] = [['empty' for _ in range(self.h)] for _ in range(self.w)]
@@ -52,6 +52,7 @@ class Game:
         assert 0 <= pos[1] <= self.h
         assert self.p_positions[idx] is None
         assert self.field[pos[0]][pos[1]] == 'empty'
+        # TODO: turn-based
         self.p_positions[idx] = (pos[0], pos[1])
         self.field[pos[0]][pos[1]] = idx
         self.p_rotations[idx] = rot
@@ -71,36 +72,94 @@ class Game:
                     elif p == 'red':
                         ch = 'rr'
                     else:
-                        ch = self.rotate_piece(self.p_descriptions[p], times=self.p_rotations[p])[i2:i2+2]
+                        ch = self.rotate_piece(self.p_descriptions[p], times=self.p_rotations[p])[i2:i2 + 2]
                     f[i * 2 + i2] += ch
         return f
 
-
-clients = {}
-games = {}
-
-@sio.event
-def connect(sid, environ):
-    client = Client(sid)
-    clients[sid] = client
-    print(f'Connected {sid} with {client.my_uuid}')
-
-@sio.event
-async def message(sid, msg):
-    msg = json.loads(msg)
-    match msg['cmd']:
-        case 'op':
-            pass
-
-    print("message recieved ", msg)
+    # TODO: scoring, disbanding and game over
 
 
-@sio.event
-def disconnect(sid):
-    print(f'Disconnected {sid} with {clients[sid].my_uuid}')
-    print('disconnect ', sid)
-    del clients[sid]
+class Server:
+    def __init__(self):
+        self.sio = socketio.AsyncServer()
+        self.app = web.Application()
+        self.sio.attach(self.app)
+        self.sio.on('connect', self.connect)
+        self.sio.on('my_message', self.my_message)
+        self.sio.on('disconnect', self.disconnect)
+
+        self.op_token = random.randint(int(1e10), int(9e10))
+        print(f'Op token is: {self.op_token}')
+        self.clients: dict[str, Client] = {}
+        self.games: dict[str, Game] = {}
+        self.games_last_id = 0
+
+    async def player_add(self, sid, game_id):
+        assert len(self.games[game_id].players) < 3
+        self.games[game_id].players.append(self.clients[sid])
+        self.clients[sid].game = game_id
+        await self.clients[sid].send_stuff({'cmd': 'msg', 'msg': f'You are now in game {game_id}'})
+
+    async def cmd_host(self, sid):
+        assert self.clients[sid].game is None
+        self.games_last_id += 1
+        now_id = str(self.games_last_id)
+        self.games[now_id] = Game()
+        await self.player_add(sid, now_id)
+        print(f' {sid} Hosted game {now_id}')
+
+    async def cmd_board(self, sid):
+        c = self.clients[sid]
+        g = self.games[c.game]
+        s = g.get_colored_state()
+        m = {'cmd': 'board', 'board': s}
+        await c.send_stuff(m)
+
+    async def cmd_put(self, sid, idx, pos, rot):
+        c = self.clients[sid]
+        g = self.games[c.game]
+        g.put_piece(idx, pos, rot)
+        await c.send_stuff({'cmd': 'msg', 'msg': 'Placement successful'})
+
+    async def connect(self, sid, environ):
+        client = Client(self.sio, sid)
+        self.clients[sid] = client
+        await self.sio.enter_room(sid, f'room{sid}')
+        await client.send_stuff({'msg': 'ok_from_server'})
+        print(f' {sid} Connected')
+
+    async def my_message(self, sid, msg):
+        msg = json.loads(msg)
+        match msg['cmd']:
+            case 'msg':
+                print(f" {sid} Tells us: '{msg['msg']}'")
+            case 'host':
+                await self.cmd_host(sid)
+            case 'join':
+                await self.player_add(sid, msg['game_id'])
+                print(f" {sid} Joined game {msg['game_id']}")
+            case 'board':
+                await self.cmd_board(sid)
+                print(f" {sid} Got board")
+            case 'put':
+                await self.cmd_put(sid, msg['idx'], msg['pos'], msg['rot'])
+                print(f" {sid} Put piece")
+            case 'op':
+                if msg['token'] == self.op_token:
+                    self.clients[sid].is_op = True
+                    print(f' {sid} OPPED')
+                else:
+                    print(f' {sid} Not opped')
+                # TODO: Maybe tell them?
+            case _:
+                raise NotImplemented('Weird command')
+
+    async def disconnect(self, sid):
+        print(f' {sid} Disconnected')
+        del self.clients[sid]
+        raise NotImplemented('Disbanding game')
 
 
 if __name__ == '__main__':
-    web.run_app(app, port=DEFAULT_PORT)
+    s = Server()
+    web.run_app(s.app, port=DEFAULT_PORT)
