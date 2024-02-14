154cf159935e855952ce51b11d2cfa11a1aaa91c
Semjon Kravtsenko <semjon.00@gmail.com>
2024-02-14 23:43:16 +0200

    Working networking
    Hopefully

--- console_client/main.py
+++ console_client/main.py
@@ -2,16 +2,17 @@
 
 import asyncio
 import json
+import websockets
+
 from constants import DEFAULT_PORT
-import asyncio, websockets, json, time
 
 
 class Client:
-    def __init__(self):
-        pass
+    def __init__(self, websocket):
+        self.websocket = websocket
 
-    async def send_stuff(self, data):
-        pass
+    async def send_stuff(self, msg):
+        await self.websocket.send(json.dumps(msg))
 
     async def cmd(self, cmd):
         match cmd[0]:
@@ -59,13 +60,13 @@ async def hello():
     if ':' not in where:
         where = f'{where}:{DEFAULT_PORT}'
     async with websockets.connect(f"ws://{where}") as websocket:
-        c = Client()
-        print("Connected")
-        await websocket.send(json.dumps({'msg': 'I am a silly boy RAWR :3'}))
+        c = Client(websocket)
+        await c.send_stuff({'cmd': 'msg', 'msg': 'I am a silly boy RAWR :3'})
         reader_task = asyncio.ensure_future(c.reader(websocket))
+        commands_task = asyncio.ensure_future(c.command_loop())
         done = await asyncio.wait(
-            [reader_task],
-            return_when=asyncio.ALL_COMPLETED,
+            [reader_task, commands_task],
+            return_when=asyncio.FIRST_COMPLETED,
         )
 
 
